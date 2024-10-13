6042d6038d073a3f29a84b811940bf78c51e068e
Sheriffuddeen09 <odukoyasheriff@gmail.com>
2024-10-13 07:50:00 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  keyframes: {
+    'open-menu': {
+      '0%': { transform: 'scaleY(0)' },
+      '80%': { transform: 'scaleY(1.2)' },
+      '100%': { transform: 'scaleY(1)' },
+    },
+  },
+  animation: {
+    'open-menu': 'open-menu 0.5s ease-in-out forwards',
+  },
+  plugins: [require("tailwind-scrollbar")],
+  variants: {
+    // ...
+    scrollbar: ['dark']
 }
-
+}
