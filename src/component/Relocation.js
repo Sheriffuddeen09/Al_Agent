84cfff3f187cba8b09078ca8ce5309326c25be9e
Sheriffuddeen09 <odukoyasheriff@gmail.com>
2024-10-14 01:14:57 +0100

    commit

--- src/component/Relocation.js
+++ src/component/Relocation.js
@@ -13,32 +13,49 @@ function Relocation (){
         })
     },[])
 
+    var counter = 1
+
+    setInterval(() => {
+        document.getElementById('radio' + counter).checked=true
+        counter++;
+        if(counter > 3){
+            counter = 1 
+        }
+    },3000);
     const contentmid = (
-        <div className="block sm:hidden container">
-            <div className="flex flex-row gap-10 text-white mt-10 slider-wrapper">
-                    <div className=" border img" id="slider-1">
+        <div className="block sm:hidden sliders">
+            <div className="flex flex-row gap-10 mb-16 text-white mt-10 slide">
+                <input type="radio" name="radio-btn" id="radio1" />
+                <input type="radio" name="radio-btn" id="radio2" />
+                <input type="radio" name="radio-btn" id="radio3" />
+                    <div className=" border image st first" id="slider-1">
                     <img alt="imagepic" src={vehicle} width={110} className="w-72 h-40  "/>
                     <h1 className="text-sm text-gray-400 mb-2 px-4">JULY 10,2024 </h1>
                     <p className="w-48 px-4" style={{fontSize:"13px"}}>Settling into a new home: Strategies for an effortless movement , hassle free.</p>
                     <p className="text-sm bg-red w-32 text-white px-4 my-2 text-center translate-x-4 p-2">Read More</p>
                     </div>
-                    <div className=" border img" id="slider-2">
+                    <div className=" border image st" id="slider-2">
                     <img alt="imagepic" src={vehicle} width={110} className="w-72 h-40 "/>
                     <h1 className="text-sm text-gray-400 mb-2 px-4">JULY 10,2024 </h1>
                     <p className="w-48 px-4" style={{fontSize:"13px"}}>Settling into a new home: Strategies for an effortless movement , hassle free.</p>
                     <p className="text-sm bg-red w-32 text-white px-4 my-2 text-center translate-x-4 p-2">Read More</p>
                     </div>
-                    <div className=" border img" id="slider-3">
+                    <div className=" border image st" id="slider-3">
                     <img alt="imagepic" src={vehicle} width={110} className="w-72 h-40 "/>
                     <h1 className="text-sm text-gray-400 mb-2 px-4">JULY 10,2024 </h1>
                     <p className="w-48 px-4" style={{fontSize:"13px"}}>Settling into a new home: Strategies for an effortless movement , hassle free.</p>
                     <p className="text-sm bg-red w-32 text-white px-4 my-2 text-center translate-x-4 p-2">Read More</p>
                     </div>
-                    <div className="slider-nav">
-                        <a href="#slider-1"></a>
-                        <a href="#slider-2"></a>
-                        <a href="#slider-3"></a>
-                    </div>
+                <div className="nav-auto">
+                    <div className="a-b1"></div>
+                    <div className="a-b2"></div>
+                    <div className="a-b3"></div>
+                </div>
+                <div className="nav-m">
+                    <label htmlFor="radio1" className="m-btn"></label>
+                    <label htmlFor="radio1" className="m-btn"></label>
+                    <label htmlFor="radio1" className="m-btn"></label>
+                </div>
             </div>
         </div>
         
